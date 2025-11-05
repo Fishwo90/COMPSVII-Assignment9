77a4ca3a708a469acafe70c3846c513254c42554
Fischer Wood <fwwood@york.edu>
2025-11-04 20:34:51 -0600

    Implement dynamic GIF search functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 console.log("script.js loaded");
  
-
-
+//constants
+const apiKey = "FEIlF3DnGPNHgjqRhrj8DZ58425QTijj";
+const apiBaseUrl = "https://api.giphy.com/v1/gifs/search";
 
 // DOM Element Selectors
 // this selects the HTML elements and stores them in variables.
@@ -11,16 +12,24 @@ const gifContainer = document.querySelector("#gif-container");
 
 // Main Function
 
-//add something to fetch later
-
-
-
 const fetchAndDisplayGifs = async () => {
+  //Get Search Term
+  const searchTerm = searchInput.value;
 
-  console.log("Search button was clicked!");
-  console.log("Search term:", searchInput.value);
-  gifContainer.innerHTML = "<p>Button clicked! Fetching...</p>";
+  // Basic validation...
+  if (!searchTerm) {
+    gifContainer.innerHTML =
+      '<p class="text-center">Please enter a search term.</p>';
+    return;
+  }
+  
+  } catch (error) {
+     //Error Handling
+    console.error("Error fetching GIFs:", error);
+    gifContainer.innerHTML = `<p class="text-center text-danger">Sorry, something went wrong. Please try again.</p>`;
+  }
 };
+
 
 // Event Listeners
 // This line "listens" for a 'click' on the search button.
